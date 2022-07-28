5a49d50df1f5f2189dc458003713f76093bba2ac
Karun S <karoon.siri@gmail.com>
2022-07-28 12:10:54 +0700

    Allow navigating away, and into project root directory
    For example, when viewing a bundle gem source. LSP could bring you to outside of project root. If you don't set 'prefer_startup_root', you'll have trouble coming back to the right root afterwards

--- config/nvim/plugin/nvim-tree.lua
+++ config/nvim/plugin/nvim-tree.lua
@@ -1,4 +1,10 @@
 require("nvim-tree").setup({
+  -- Prefer startup root directory when updating root directory of the tree.
+  -- Only relevant when `update_focused_file.update_root` is `true`
+  prefer_startup_root = true,
+  update_focused_file = {
+    update_root = true,
+  },
   view = {
     adaptive_size = true,
     mappings = {
@@ -15,7 +21,6 @@ require("nvim-tree").setup({
     change_dir = {
       enable = true,
       global = true,
-      restrict_above_cwd = false,
     },
   }
 })
