c3cb4049830b63df249849884b07eab364eb6b94
Karun S <karoon.siri@gmail.com>
2022-08-11 18:58:19 +0700

    Show diagnostics & completions for ruby

--- config/nvim/plugin/lsp/ruby.lua
+++ config/nvim/plugin/lsp/ruby.lua
@@ -1,6 +1,13 @@
 local configs = require('configs/lsp')
 
 require('lspconfig').solargraph.setup({
+  cmd = { "bin/solargraph", "stdio" },
+  settings = {
+    solargraph = {
+      diagnostics = true,
+      completion = true,
+    }
+  },
   on_attach = configs.on_attach,
   capabilities = configs.capabilities,
 })
