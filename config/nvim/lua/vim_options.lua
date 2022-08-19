8d088665f4e45611b13075a0ab744a81f4ef01c3
Karun S <karoon.siri@gmail.com>
2022-08-19 22:22:11 +0700

    Reformat vim_options.lua

--- config/nvim/lua/vim_options.lua
+++ config/nvim/lua/vim_options.lua
@@ -1,85 +1,90 @@
 local o = vim.opt
+local options = {
+  -- Automatically :write before running commands
+  autowrite = true,
 
-if vim.fn.has('termguicolors') then
-  o.termguicolors = true
+  -- Backspace deletes like most programs in insert mode
+  backspace = { 'indent', 'eol', 'start' },
+  backup = false,
+  colorcolumn = '+1',
+  expandtab = true,
+  history = 50,
+  ignorecase = true,
+  incsearch = true,
+  joinspaces = false,
+  number = true,
+  relativenumber = true,
+  list = true,
+  listchars = 'tab:»·,trail:·,nbsp:·',
+  shiftround = true,
+  shiftwidth = 2,
+  smartcase = true,
+  softtabstop = 2,
+  tabstop = 2,
+  textwidth = 80,
+  wrap = false,
+  writebackup = false,
+  autoindent = true,
+  formatoptions = o.formatoptions - { 't' },
+  cursorline = true,
+  completeopt = 'menu,menuone,noselect',
+
+  -- Always display the status line
+  laststatus = 2,
+
+  -- Disable modelines as a security precaution
+  modelines = 0,
+  modeline = false,
+
+  -- Show the cursor position all the time
+  ruler = true,
+
+  -- Display incomplete commands
+  showcmd = true,
+
+  -- http://robots.thoughtbot.com/post/18739402579/global-gitignore#comment-458413287
+  swapfile = false,
+
+  -- Set default foldmethod. Not every syntax has folding set
+  foldmethod = 'indent',
+  foldnestmax = 10,
+  foldenable = false,
+  foldlevel = 1,
+  showmode = false,
+
+  -- Tab completion
+  -- will insert tab at beginning of line,
+  -- will use completion if not at beginning
+  wildmode= { 'list:longest', 'list:full' },
+
+  -- Set tags for vim-fugitive
+  -- TODO: Check this
+  tags = o.tags ^ { '.git/tags' },
+  splitbelow = true,
+  splitright = true,
+
+  -- Set spellfile to location that is guaranteed to exist, can be symlinked to
+  -- Dropbox or kept in Git and managed outside of thoughtbot/dotfiles using rcm.
+  spellfile = '$HOME/.vim-spell-en.utf-8.add',
+
+  -- Use Ag over Grep
+  grepprg = 'ag --nogroup --nocolor',
+}
+
+for k, v in pairs(options) do
+  o[k] = v
 end
 
--- Automatically :write before running commands
-o.autowrite = true
-
--- Backspace deletes like most programs in insert mode
-o.backspace = { 'indent', 'eol', 'start' }
-o.backup = false
-o.colorcolumn = '+1'
-o.expandtab = true
-o.history = 50
-o.ignorecase = true
-o.incsearch = true
-o.joinspaces = false
-o.number = true
-o.relativenumber = true
-o.list = true
-o.listchars = 'tab:»·,trail:·,nbsp:·'
-o.shiftround = true
-o.shiftwidth = 2
-o.smartcase = true
-o.softtabstop = 2
-o.tabstop = 2
-o.textwidth = 80
-o.wrap = false
-o.writebackup = false
-o.autoindent = true
-o.formatoptions = o.formatoptions - { 't' }
-o.cursorline = true
-o.completeopt = 'menu,menuone,noselect'
 o.sessionoptions:append { 'globals' }
-
--- Always display the status line
-o.laststatus = 2
-
--- Disable modelines as a security precaution
-o.modelines = 0
-o.modeline = false
-
--- Show the cursor position all the time
-o.ruler = true
-
--- Display incomplete commands
-o.showcmd = true
-
--- http://robots.thoughtbot.com/post/18739402579/global-gitignore#comment-458413287
-o.swapfile = false
-
--- Set default foldmethod. Not every syntax has folding set
-o.foldmethod = 'indent'
-o.foldnestmax = 10
-o.foldenable = false
-o.foldlevel = 1
-o.showmode = false
-
--- Tab completion
--- will insert tab at beginning of line,
--- will use completion if not at beginning
-o.wildmode= { 'list:longest', 'list:full' }
-
--- Set tags for vim-fugitive
--- TODO: Check this
-o.tags = o.tags ^ { '.git/tags' }
-o.splitbelow = true
-o.splitright = true
-
--- Set spellfile to location that is guaranteed to exist, can be symlinked to
--- Dropbox or kept in Git and managed outside of thoughtbot/dotfiles using rcm.
-o.spellfile = '$HOME/.vim-spell-en.utf-8.add'
-
 -- Autocomplete with dictionary words when spell check is on
 o.complete:append { 'kspell' }
 
 -- Always use vertical diffs
 o.diffopt:append { 'vertical' }
 
--- Use Ag over Grep
-o.grepprg = 'ag --nogroup --nocolor'
+if vim.fn.has('termguicolors') then
+  o.termguicolors = true
+end
 
 -- Use ag in fzf for listing files. Lightning fast and respects .gitignore
 vim.env.FZF_DEFAULT_COMMAND = 'ag --literal --files-with-matches --nocolor --hidden -g ""'
