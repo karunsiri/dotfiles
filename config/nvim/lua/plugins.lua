37818dfc1018e14ae8588fe5e7d2aea8f6663aa9
Karun S <karoon.siri@gmail.com>
2022-09-09 23:34:32 +0700

    Bump bufferline to v2.9.1

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -28,7 +28,7 @@ return require('packer').startup(function()
   -- Aesthetics
   use 'joshdick/onedark.vim'
   use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use { 'akinsho/bufferline.nvim', tag = 'v2.8.1' }
+  use { 'akinsho/bufferline.nvim', tag = 'v2.9.1' }
   use 'glepnir/dashboard-nvim'
   use 'hzchirs/vim-material'
   use {
