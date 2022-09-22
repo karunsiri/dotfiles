3c55108288b0525776edcbae9bb0ba8bcb3c22c2
Karun S <karoon.siri@gmail.com>
2022-09-22 14:15:42 +0700

    Simplify gitsigns mapping

--- config/nvim/plugin/gitsigns.lua
+++ config/nvim/plugin/gitsigns.lua
@@ -18,7 +18,7 @@ end
 
 gitsigns.setup({
   on_attach = function(bufnr)
-    local gs = require('gitsigns')
+    local gs = package.loaded.gitsigns
 
     map('n', '<Leader>db', function() gs.blame_line() end)
     map('n', '<Leader>dd', function() gs.diffthis() end)
@@ -26,6 +26,7 @@ gitsigns.setup({
     -- Hunk actions
     map('n', '<Leader>dp', function() gs.preview_hunk() end)
     map('n', '<Leader>ds', function() gs.stage_hunk() end)
+    map('n', '<Leader>ds', gs.stage_hunk)
     map('n', '<Leader>du', function() gs.undo_stage_hunk() end)
   end
 })
