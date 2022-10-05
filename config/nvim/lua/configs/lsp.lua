6723910547569b00741a976423182ba2d47ca684
Karun S <karoon.siri@gmail.com>
2022-10-05 12:31:06 +0700

    Replace <Space> with <Leader> for nvim mappings

--- config/nvim/lua/configs/lsp.lua
+++ config/nvim/lua/configs/lsp.lua
@@ -3,7 +3,7 @@ local M = {} -- module to export. Same style as ES6 module.
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<Space>e', function()
+vim.keymap.set('n', '<Leader>e', function()
   require('lspsaga.diagnostic').show_line_diagnostics()
 end, opts)
 
@@ -22,17 +22,17 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-s>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
+  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<Leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
+  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<Leader>ca', '<cmd>Lspsaga code_action<CR>', bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<Leader>gr', '<cmd>Lspsaga lsp_finder<CR>', bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<Leader>f', vim.lsp.buf.format, bufopts)
 end
 
 -- Setup lspconfig.
