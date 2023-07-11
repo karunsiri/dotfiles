31e9be9a47ffed83fd684fc4bcebf30f33061f5d
Karun S <karoon.siri@gmail.com>
2023-07-11 22:06:51 +0700

    Temporarily disable tailwindcss LSP

--- config/nvim/plugin/lsp/tailwindcss.lua
+++ config/nvim/plugin/lsp/tailwindcss.lua
@@ -1,5 +1,9 @@
 local configs = require('configs/lsp')
 
+if true then
+  return
+end
+
 require('lspconfig').tailwindcss.setup({
   on_attach = configs.on_attach,
   capabilities = configs.capabilities,
