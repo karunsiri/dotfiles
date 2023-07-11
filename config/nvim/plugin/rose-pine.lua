92804a2d0be98ed4636230a7a129a03877d870a6
Karun S <karoon.siri@gmail.com>
2023-07-11 23:38:01 +0700

    Custom rose-pine highlights
    - TSX
    - Ruby
    - Terraform

--- config/nvim/plugin/rose-pine.lua
+++ config/nvim/plugin/rose-pine.lua
@@ -8,5 +8,24 @@ rose.setup({
   highlight_groups = {
 		IndentBlanklineChar = { fg = 'highlight_med' },
     IndentBlanklineContextChar = { fg = 'rose' },
+
+    -- TSX
+    ['@variable.tsx'] = { link = null },
+    ['@constructor.tsx'] = { fg = 'rose', italic = false },
+    ['@include.tsx'] = { fg = 'love' },
+    ['@keyword.tsx'] = { fg = 'love' },
+    ['@type.tsx'] = { fg = 'text', italic = false },
+    ['@tag.attribute.tsx'] = { fg = 'foam' },
+
+    -- Ruby
+    ['@variable.ruby'] = { link = null },
+    ['@symbol.ruby'] = { fg = 'gold' },
+    ['@punctuation.bracket.ruby'] = { fg = 'iris' },
+    ['@keyword.ruby'] = { fg = 'love' },
+    ['@keyword.function.ruby'] = { fg = 'love' },
+
+    -- Terraform
+    ['@variable.terraform'] = { link = null },
+    ['@lsp.type.type.terraform'] = { fg = 'love' },
 	}
 })
