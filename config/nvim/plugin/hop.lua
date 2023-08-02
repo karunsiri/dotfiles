f9fde6277425a950cdaf036ec29abce62cbe41ba
Karun S <karoon.siri@gmail.com>
2023-08-02 14:31:24 +0700

    map (n,v) F to hop search by word

--- config/nvim/plugin/hop.lua
+++ config/nvim/plugin/hop.lua
@@ -7,4 +7,4 @@ hop.setup()
 
 local opts = { noremap = true }
 vim.keymap.set({ 'n', 'v' }, 'f', hop.hint_char1, opts)
-vim.keymap.set('n', '<Leader><Leader>w', hop.hint_words, opts)
+vim.keymap.set({ 'n', 'v' }, 'F', hop.hint_words, opts)
