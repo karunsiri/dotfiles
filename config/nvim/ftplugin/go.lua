0f82ee1836ad855aa87e1a418152bbf02a109716
Karun S <karoon.siri@gmail.com>
2023-10-11 23:17:54 +0700

    Use gopls as go LSP

--- config/nvim/ftplugin/go.lua
+++ config/nvim/ftplugin/go.lua
@@ -1,10 +1,11 @@
-vim.g.go_fmt_command = 'goimports'
-
 vim.o.listchars = 'tab:  ,trail:·,nbsp:·'
 vim.bo.expandtab = false
 vim.cmd[[compiler go]]
 
 vim.api.nvim_create_autocmd(
   { 'BufWritePre' },
-  { buffer = 0, callback = vim.lsp.buf.formatting_sync }
+  {
+    buffer = 0,
+    callback = function() vim.lsp.buf.format() end,
+  }
 )
