d638e5f99270c22c06a56fe12f9da50ee8c9cb97
Karun S. <karoon.siri@gmail.com>
2026-02-21 21:14:59 +0700

    Loads ASDF shim first before system

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,5 @@
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+PATH="$HOME/.bin:$PATH:/usr/local/sbin"
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
