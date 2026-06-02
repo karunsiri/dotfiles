febc63ed0414d8b30f3785cac0f62a993a689552
Karun S. <karoon.siri@gmail.com>
2026-06-02 15:53:04 +0700

    add mise shims to PATH if mise is installed

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -4,4 +4,9 @@ PATH="$HOME/.bin:$PATH:/usr/local/sbin"
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+# Ensure mise shims are available, but only if mise is installed
+if [[ -d "$HOME/.local/share/mise/shims" ]]; then
+  PATH="$PATH:$HOME/.local/share/mise/shims"
+fi
+
 export -U PATH
